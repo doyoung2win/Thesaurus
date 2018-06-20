894965966c8c613c4674aa614cd58f1765aadcdf
Do Suh <doyoung.suh@gmail.com>
2018-06-19 19:47:26 -0700

    thesaurus file created

---
+++ Thesaurus/Thesaurus.swift
@@ -0,0 +1,20 @@
+//
+//  Thesaurus.swift
+//  Thesaurus
+//
+//  Created by Do on 6/19/18.
+//  Copyright © 2018 Do. All rights reserved.
+//
+
+import Foundation
+
+
+var synonyms = ["swift" : ["abrupt", "expeditious", "hasty", "nimble", "quick", "rapid", "speedy", "sudden", "unexpected"], "objective" : ["detached", "disinterested", "dispassionate", "equitable", "evenhanded", "nonpartisan", "open-minded", "unbiased"], "calculate" : ["adjust", "appraise", "consider", "count", "determine", "forecast", "gauge", "guess", "measure", "multiply", "reckon", "subtract", "tally", "weigh", "work out"], "create" : ["build", "conceive", "constitute", "construct", "design", "devise", "discover", "establish", "forge", "form"]]
+
+func synonyms(_ input: String) {
+    if let wordSynonym = synonyms[input] {
+        print(wordSynonym)
+    } else {
+        print("Sorry I only have a few words in my thesaurus!  Some of these include swift, objective, calculate and create.")
+    }
+}
--- Thesaurus/ViewController.swift
+++ Thesaurus/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func showSynonym(_ sender: Any) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
 }
 
